7a44004e8f9e9f178b8499a449e10d2e07f2b00f
Sean Murphy <sean@iamseanmurphy.com>
2024-09-23 04:18:55 +0000

    2.0.1

---
+++ wc-min-max-quantities/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'pluginever/wc-min-max-quantities',
+        'pretty_version' => 'v2.0.1',
+        'version' => '2.0.1.0',
+        'reference' => '064ae6d386fd43db20b39cafce227a9296012134',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'pluginever/wc-min-max-quantities' => array(
+            'pretty_version' => 'v2.0.1',
+            'version' => '2.0.1.0',
+            'reference' => '064ae6d386fd43db20b39cafce227a9296012134',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

88e5b03dc2f9c1e553b22a715cfe5ff928adf9a3
Sean Murphy <sean@iamseanmurphy.com>
2025-03-04 06:19:44 +0000

    2.0.8

--- wc-min-max-quantities/vendor-prefixed/autoload-classmap.php
+++ wc-min-max-quantities/vendor-prefixed/autoload-classmap.php
@@ -5,13 +5,13 @@
 $strauss_src = dirname(__FILE__);
 
 return array(
-   'WooCommerceMinMaxQuantities\ByteKit\Admin\Settings' => $strauss_src . '/byteever/bytekit-settings/src/Admin/Settings.php',
    'WooCommerceMinMaxQuantities\ByteKit\Plugin' => $strauss_src . '/byteever/bytekit-plugin/src/Plugin.php',
-   'WooCommerceMinMaxQuantities\ByteKit\Admin\Notices' => $strauss_src . '/byteever/bytekit-plugin/src/Admin/Notices.php',
-   'WooCommerceMinMaxQuantities\ByteKit\Admin\Flash' => $strauss_src . '/byteever/bytekit-plugin/src/Admin/Flash.php',
    'WooCommerceMinMaxQuantities\ByteKit\Traits\HasPlugin' => $strauss_src . '/byteever/bytekit-plugin/src/Traits/HasPlugin.php',
-   'WooCommerceMinMaxQuantities\ByteKit\Scripts' => $strauss_src . '/byteever/bytekit-plugin/src/Scripts.php',
-   'WooCommerceMinMaxQuantities\ByteKit\Interfaces\Scriptable' => $strauss_src . '/byteever/bytekit-plugin/src/Interfaces/Scriptable.php',
-   'WooCommerceMinMaxQuantities\ByteKit\Interfaces\Pluginable' => $strauss_src . '/byteever/bytekit-plugin/src/Interfaces/Pluginable.php',
    'WooCommerceMinMaxQuantities\ByteKit\Services' => $strauss_src . '/byteever/bytekit-plugin/src/Services.php',
+   'WooCommerceMinMaxQuantities\ByteKit\Admin\Flash' => $strauss_src . '/byteever/bytekit-plugin/src/Admin/Flash.php',
+   'WooCommerceMinMaxQuantities\ByteKit\Admin\Notices' => $strauss_src . '/byteever/bytekit-plugin/src/Admin/Notices.php',
+   'WooCommerceMinMaxQuantities\ByteKit\Interfaces\Pluginable' => $strauss_src . '/byteever/bytekit-plugin/src/Interfaces/Pluginable.php',
+   'WooCommerceMinMaxQuantities\ByteKit\Interfaces\Scriptable' => $strauss_src . '/byteever/bytekit-plugin/src/Interfaces/Scriptable.php',
+   'WooCommerceMinMaxQuantities\ByteKit\Scripts' => $strauss_src . '/byteever/bytekit-plugin/src/Scripts.php',
+   'WooCommerceMinMaxQuantities\ByteKit\Admin\Settings' => $strauss_src . '/byteever/bytekit-settings/src/Admin/Settings.php',
 );
--- wc-min-max-quantities/vendor/composer/installed.php
+++ wc-min-max-quantities/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'pluginever/wc-min-max-quantities',
-        'pretty_version' => 'v2.0.7',
-        'version' => '2.0.7.0',
-        'reference' => '4fe53e16fb13121af84fe6bebf7d05d37609f53d',
+        'pretty_version' => 'v2.0.8',
+        'version' => '2.0.8.0',
+        'reference' => '44eff79e64aeafdd17cc00568399c474601d4b93',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'byteever/bytekit-plugin' => array(
-            'pretty_version' => 'v1.0.1',
-            'version' => '1.0.1.0',
-            'reference' => '38f7aa0311e49a33d8f62e54b9fc118eb1ad32b5',
+            'pretty_version' => 'v1.0.2',
+            'version' => '1.0.2.0',
+            'reference' => '794ec9834ecac6cead6b1064a605ff930864950b',
             'type' => 'library',
             'install_path' => __DIR__ . '/../byteever/bytekit-plugin',
             'aliases' => array(),
@@ -29,9 +29,9 @@
             'dev_requirement' => false,
         ),
         'pluginever/wc-min-max-quantities' => array(
-            'pretty_version' => 'v2.0.7',
-            'version' => '2.0.7.0',
-            'reference' => '4fe53e16fb13121af84fe6bebf7d05d37609f53d',
+            'pretty_version' => 'v2.0.8',
+            'version' => '2.0.8.0',
+            'reference' => '44eff79e64aeafdd17cc00568399c474601d4b93',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
